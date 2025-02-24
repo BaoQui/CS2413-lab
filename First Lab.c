88c6070260442cf9834397e9432088a334532af7
BaoQui <102458754+BaoQui@users.noreply.github.com>
2025-02-23 22:13:12 -0600

    Update First Lab.c

--- First Lab.c
+++ First Lab.c
@@ -1,3 +1,4 @@
+//Reverse List
 struct ListNode* reverseList(struct ListNode* head) 
 {
     struct ListNode *previous = NULL, *current = head, *next = NULL;
@@ -11,4 +12,32 @@ struct ListNode* reverseList(struct ListNode* head)
     }
 
     return previous;
+}
+
+//Remove Linked List Element
+struct ListNode* removeElements(struct ListNode* head, int val) 
+{
+    struct ListNode temp;
+    temp.next = head;
+    struct ListNode *previous = &temp, *current = head;
+
+    while(current)
+    {
+        struct ListNode *nxt = current->next;
+
+        if (current->val == val)
+        {
+            previous->next = nxt;
+            free(current);
+
+        }
+        else
+        {
+            previous = current;
+        }
+
+        current = nxt;
+    }
+
+    return temp.next;
 }
